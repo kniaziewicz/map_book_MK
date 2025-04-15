1e12407e289f5f80d18ae41253a6ea5b23d64892
kniaziewicz <michalkak@onet.pl>
2025-04-15 19:10:59 +0200

    wip: update user functionality

--- utils/controler.py
+++ utils/controler.py
@@ -13,3 +13,11 @@ def remove_user(users_data: list)->None:
     for user in users_data:
         if user['name'] == user_name:
             users_data.remove(user)
+
+def update_user(users_data: list)->None:
+    user_name:str=input('Podaj imię znajomego do aktualizacji: ')
+    for user in users_data:
+        if user['name'] == user_name:
+            user['name']=input('Podaj nowe imię znajomego: ')
+            user['location']=input('Podaj nową miejscowość: ')
+            user['posts']=input('Podaj nową ilość postów: ')
